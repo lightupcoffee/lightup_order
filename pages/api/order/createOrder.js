79b99076521a64aafa95f3243f74d5f1a540ad6f
KINGWAY\bill_hsu <bill_hsu@kingwaytek.com>
2024-02-21 13:48:30 +0800

    修正createOrder錯誤

--- pages/api/order/createOrder.js
+++ pages/api/order/createOrder.js
@@ -1,13 +1,12 @@
 import { db } from '../../../db.js'
 export default async function createOrder(req, res) {
+  const client = await db.connect()
   try {
     if (req.method !== 'POST') {
       // 處理非 POST 請求
       res.setHeader('Allow', ['POST'])
       res.status(405).end(`Method ${req.method} Not Allowed`)
     }
-
-    const client = await db.connect()
 
     const { car, tableid } = req.body
     let totalAmount = 0
@@ -28,32 +27,22 @@ export default async function createOrder(req, res) {
       totalAmount += product.price * car[key].count
       return orderitem
     })
-    console.log('json', JSON.stringify(orderitemlist))
-    const addOrderAndItems = async () => {
-      try {
-        await client.query('BEGIN')
 
-        //建立訂單並回傳訂單id
-        await client.query(
-          `INSERT INTO lightup."Order" (totalamount, status, tableid, createtime,item)
-         VALUES ('${totalAmount}', 0, '${tableid}', NOW(),'${JSON.stringify(orderitemlist)}')`,
-        )
+    // 建立訂單並處理事務
+    await client.query('BEGIN')
 
-        await client.query('COMMIT')
-      } catch (e) {
-        await client.query('ROLLBACK')
-        throw e
-      }
-    }
-    addOrderAndItems().catch((err) => {
-      console.log(err)
-      return res.status(500).json({ error: err.message })
-    })
-    client.release()
+    await client.query(
+      `INSERT INTO lightup."Order" (totalamount, status, tableid, createtime, item)
+       VALUES ('${totalAmount}', 0, '${tableid}', NOW(), '${JSON.stringify(orderitemlist)}')`,
+    )
 
-    return res.status(200).json({ success: true })
+    await client.query('COMMIT')
+    res.status(200).json({ success: true })
   } catch (err) {
     console.log(err)
+    await client.query('ROLLBACK')
     res.status(500).json({ error: err.message })
+  } finally {
+    client.release() // 確保在操作完畢後釋放連接
   }
 }
