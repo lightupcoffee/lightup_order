b67a749ceb529687277263eaf30dade150391921
KINGWAY\bill_hsu <bill_hsu@kingwaytek.com>
2024-03-12 22:31:37 +0800

    更新類別和品項排序

--- pages/api/product/getAllProduct.js
+++ pages/api/product/getAllProduct.js
@@ -4,7 +4,7 @@ import { db } from '../../../db.js'
 export default async function getAllProduct(req, res) {
   try {
     const client = await db.connect()
-    const result = await client.query('SELECT * FROM lightup."Product" where active=true order by productid')
+    const result = await client.query('SELECT * FROM lightup."Product" where active=true order by sort')
     res.status(200).json(result.rows)
     client.release()
   } catch (err) {
